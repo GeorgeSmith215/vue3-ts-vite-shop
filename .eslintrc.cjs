120c0a16dae0fb4f5b62a4f1fb2749ba312b2d29
GeorgeSmith <georgesmith0215@outlook.com>
2024-02-14 21:34:57 +0800

    chore: add commitlint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,26 +1,30 @@
-// .eslintrc.js
 module.exports = {
-    env: {
-      browser: true,
-      es2021: true
-    },
-    extends: [
-      // 'plugin:vue/essential',
-      
-      // 使用 Vue 3 规则
-      // https://eslint.vuejs.org/user-guide/#bundle-configurations
-      'plugin:vue/vue3-strongly-recommended',
-      'standard'
-    ],
-    parserOptions: {
-      ecmaVersion: 12,
-      parser: '@typescript-eslint/parser',
-      sourceType: 'module'
-    },
-    plugins: [
-      'vue',
-      '@typescript-eslint'
-    ],
-    rules: {}
-  }
-  
+  globals: {
+    defineProps: 'readonly',
+    defineEmits: 'readonly',
+    defineExpose: 'readonly',
+    withDefaults: 'readonly'
+  },
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ['plugin:vue/vue3-strongly-recommended', 'standard'],
+  parserOptions: {
+    ecmaVersion: 12,
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module'
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {
+    'vue/no-mutating-props': 'off'
+  },
+  overrides: [
+    {
+      files: ['src/api/**/*.ts'],
+      rules: {
+        camelcase: 'off'
+      }
+    }
+  ]
+}
